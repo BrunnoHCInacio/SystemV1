43e712e9c52c49abe34db7bccc0139eed0cbe842
Brunno Henrique Campos Inácio <brunnoh11@gmail.com>
2021-03-16 03:49:29 -0300

    Adicionado: Implementação de UnitOfWork.

---
+++ SystemV1.Domain.Core/Interfaces/Uow/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemV1.Domain.Core.Interfaces.Uow
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        void RoolBack();
+    }
+}
---
+++ SystemV1.Infrastructure/Data/Uow/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+using SystemV1.Domain.Core.Interfaces.Uow;
+
+namespace SystemV1.Infrastructure.Data.Uow
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SqlContext _sqlContext;
+
+        public UnitOfWork(SqlContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+
+        public void Commit()
+        {
+            _sqlContext.SaveChanges();
+        }
+
+        public void RoolBack()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
